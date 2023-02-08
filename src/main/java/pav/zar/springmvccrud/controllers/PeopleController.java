10a4d38a587b5c59830d1498e5a04983cbeddecf
Pavel <pavelzarov@yahoo.com>
2023-02-08 14:36:12 +0200

    Form validation on pages edit and new using hibernate validator, thymeleaf, Valid Annotation

--- src/main/java/pav/zar/springmvccrud/controllers/PeopleController.java
+++ src/main/java/pav/zar/springmvccrud/controllers/PeopleController.java
@@ -3,9 +3,12 @@ package pav.zar.springmvccrud.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import pav.zar.springmvccrud.dao.PersonDAO;
 import pav.zar.springmvccrud.models.Person;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -40,7 +43,11 @@ public class PeopleController {
 
     //maps fields in person object and saves
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if(bindingResult.hasErrors()){
+            return "people/new";
+        }
         personDAO.save(person);
         return "redirect:/people";
     }
@@ -54,7 +61,11 @@ public class PeopleController {
 
     //update specific person via patch method
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+    public String update(@ModelAttribute("person") @Valid Person person,BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if(bindingResult.hasErrors()){
+            return "people/edit";
+        }
         personDAO.update(id, person);
         return "redirect:/people";
     }
