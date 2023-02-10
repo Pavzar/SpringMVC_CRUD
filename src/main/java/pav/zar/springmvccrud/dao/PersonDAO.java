ad66f36366524ef2c0b467e01101b3661657db5e
Pavel <pavelzarov@yahoo.com>
2023-02-10 16:18:21 +0200

    refactored method to show specific person using prepared statement and jdbc

--- src/main/java/pav/zar/springmvccrud/dao/PersonDAO.java
+++ src/main/java/pav/zar/springmvccrud/dao/PersonDAO.java
@@ -58,7 +58,29 @@ public class PersonDAO {
     }
 
     public Person show(int id) {
-        return null;
+
+        Person person = null;
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM Person WHERE id = ?");
+            preparedStatement.setInt(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            resultSet.next();
+
+            person = new Person();
+
+            person.setId(resultSet.getInt("id"));
+            person.setName(resultSet.getString("name"));
+            person.setAge(resultSet.getInt("age"));
+            person.setEmail(resultSet.getString("email"));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return person;
     }
 
     public void save(Person person) {
@@ -67,7 +89,7 @@ public class PersonDAO {
             PreparedStatement preparedStatement =
                     connection.prepareStatement("INSERT INTO Person VALUES (1, ?, ?, ?)");
             preparedStatement.setString(1, person.getName());
-            preparedStatement.setInt(2,person.getAge());
+            preparedStatement.setInt(2, person.getAge());
             preparedStatement.setString(3, person.getEmail());
 
             preparedStatement.executeUpdate();
