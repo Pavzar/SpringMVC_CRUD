5b1dc58492ce922ee0ad47d7374d1c4cb8aa9d2a
Pavel <pavelzarov@yahoo.com>
2023-02-13 23:18:32 +0200

    address field pattern validation using Pattern annotation and regexp

--- src/main/java/pav/zar/springmvccrud/models/Person.java
+++ src/main/java/pav/zar/springmvccrud/models/Person.java
@@ -1,10 +1,7 @@
 package pav.zar.springmvccrud.models;
 
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 public class Person {
 
@@ -18,16 +15,20 @@ public class Person {
     @Email(message = "Email should be valid")
     private String email;
 
-    public Person(int id, String name, int age, String email) {
+    //Country, City, index(6 numbers)
+    @Pattern(regexp = "[A-Z]\\w+, [A-Z]\\w+, \\d{5}",
+            message = "Your address should be in this format: Country, City, Postal Code (5 digits)")
+    private String address;
+
+    public Person(int id, String name, int age, String email, String address) {
         this.id = id;
         this.name = name;
         this.age = age;
         this.email = email;
+        this.address = address;
     }
 
     public Person(){}
-
-
     public int getAge() {
         return age;
     }
@@ -63,4 +64,11 @@ public class Person {
     }
 
 
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
